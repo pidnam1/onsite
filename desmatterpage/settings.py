aa64b31f5f43979d974be9f9cb04390b731705c9
pidnam1 <67039156+pidnam1@users.noreply.github.com>
2021-08-09 21:29:52 -0400

    static root

--- desmatterpage/settings.py
+++ desmatterpage/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGOUT_REDIRECT_URL = '/'
 
 LOGIN_REDIRECT_URL = '/service/detail/'
